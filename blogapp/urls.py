8d64590f40420b8906698439508291ebb3fe795d
sandygudie <goodnewssandy@gmail.com>
2024-12-30 22:30:53 +0100

    updated urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-     path('<str:slug>', views.post, name='post'),
+    #  path('<str:slug>', views.post, name='post'),
+     path('post/<str:slug>/', views.post, name='post'),
 ]
