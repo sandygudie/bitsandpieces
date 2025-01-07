d92c534629e8137b4431a955000efe866f50d5fd
sandygudie <goodnewssandy@gmail.com>
2025-01-07 09:33:38 +0100

    updated database in settings

--- blog/settings.py
+++ blog/settings.py
@@ -83,11 +83,18 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 #     )
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Replace it with your DATABASES.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
